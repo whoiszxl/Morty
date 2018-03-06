d59a367d571b911203b2bd76e5fe82a84842984f
zhouxiaolong <zhouxl@lamezhi.com>
2018-03-06 12:29:13 +0800

    project version manage

--- code/web/index.php
+++ code/web/index.php
@@ -9,4 +9,13 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
+//加入版本号 RELEASE_VERSION
+if(file_exists(__DIR__."/version_book.txt")) {
+    define("RELEASE_VERSION",trim(file_get_contents(__DIR__."/version_book.txt")));
+}else{
+    define("RELEASE_VERSION",time());
+}
+
+echo RELEASE_VERSION;exit;
+
 (new yii\web\Application($config))->run();
