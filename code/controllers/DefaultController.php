199ac815ceb44b8375c7d5163494e5a5acfd7f11
zhouxiaolong <whoiszxl@gmail.com>
2018-03-10 23:14:04 +0800

    captcha function

--- code/controllers/DefaultController.php
+++ code/controllers/DefaultController.php
@@ -2,15 +2,21 @@
 
 namespace app\controllers;
 
-use Yii;
-use yii\web\Controller;
 use app\common\components\BaseWebController;
+use app\common\services\captcha\ValidateCode;
 
-class DefaultController extends BaseWebController
-{
+class DefaultController extends BaseWebController{
     
+    private $captcha_cookie_name = "validate_code";
+
     public function actionIndex() {
         //$this->layout = false;
         return $this->render("index");
     }
+
+    public function actionImg_captcha(){
+		$font_path = \Yii::$app->getBasePath().'/web/fonts/captcha.ttf';
+		$captcha_handle = new ValidateCode( $font_path );
+		$captcha_handle->doimg();
+	}
 }
